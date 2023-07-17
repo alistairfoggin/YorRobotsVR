a7e6c53bbff4415e15aad13fe8a39f44ed15f59d
Alistair Foggin <ajf582@york.ac.uk>
2023-07-17 17:37:47 +0100

    Start decal projection

--- Assets/CameraDecal.cs
+++ Assets/CameraDecal.cs
@@ -1,8 +1,12 @@
+
 using RosMessageTypes.Sensor;
+using System;
 using Unity.Robotics.ROSTCPConnector;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-public class CameraPreview : MonoBehaviour
+
+public class CameraDecal : MonoBehaviour
 {
     ROSConnection m_ROSConnection;
     Material m_Material;
@@ -12,9 +16,17 @@ public class CameraPreview : MonoBehaviour
     void Start()
     {
         m_ROSConnection = ROSConnection.GetOrCreateInstance();
-        m_Material = GetComponent<MeshRenderer>().material;
+        m_Material = new Material(Shader.Find("Shader Graphs/ProjectorImage"));
+        GetComponent<DecalProjector>().material = m_Material;
 
         m_ROSConnection.Subscribe<ImageMsg>("/camera/image_raw", UpdateImage);
+        m_ROSConnection.Subscribe<CameraInfoMsg>("/camera/camera_info", UpdateProjection);
+    }
+
+    private void UpdateProjection(CameraInfoMsg msg)
+    {
+        Matrix4x4 msgMatrix = Matrix4x4.Perspective((float)msg.K[0], 1, 0.1f, 100);
+        m_Material.SetMatrix("_Matrix4x4", msgMatrix.inverse);
     }
 
     private void UpdateImage(ImageMsg msg)
@@ -24,7 +36,7 @@ public class CameraPreview : MonoBehaviour
             m_Texture = new Texture2D((int)msg.width, (int)msg.height, TextureFormat.RGB24, true);
             m_Texture.wrapMode = TextureWrapMode.Clamp;
             m_Texture.filterMode = FilterMode.Trilinear;
-            m_Material.mainTexture = m_Texture;
+            m_Material.SetTexture("_Image", m_Texture);
         }
         else if (msg.width != m_Texture.width || msg.height != m_Texture.height)
         {
