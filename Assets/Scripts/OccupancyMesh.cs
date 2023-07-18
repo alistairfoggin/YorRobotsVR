005d62b67960a5c378ff7a6d32adf49f8ce43b0f
Alistair Foggin <ajf582@york.ac.uk>
2023-07-18 17:03:37 +0100

    Rewrite mesh to be vertical

--- Assets/Scripts/OccupancyMesh.cs
+++ Assets/Scripts/OccupancyMesh.cs
@@ -38,7 +38,7 @@ class OccupancyMeshGenerator
     private NativeArray<sbyte> data;
     private NativeArray<Vector3> vertexBuffer;
     private NativeArray<Vector2> uvBuffer;
-    private NativeArray<int> triangleBuffer;
+    private NativeList<int> triangleBuffer;
 
     public Mesh mesh { get; private set; }
     public static OccupancyMeshGenerator GetOrCreateInstance()
@@ -51,6 +51,7 @@ class OccupancyMeshGenerator
         return meshGenerator;
     }
 
+
     private OccupancyMeshGenerator()
     {
         m_ROSConnection = ROSConnection.GetOrCreateInstance();
@@ -59,8 +60,21 @@ class OccupancyMeshGenerator
         m_TFSystem = TFSystem.GetOrCreateInstance();
 
         mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32; // This has limited support on lower end platforms
     }
 
+    ~OccupancyMeshGenerator()
+    {
+        m_ROSConnection.Unsubscribe("/map");
+        if (handle != null)
+        {
+            handle.Value.Complete();
+            data.Dispose();
+            vertexBuffer.Dispose();
+            uvBuffer.Dispose();
+            triangleBuffer.Dispose();
+        }
+    }
 
     private void UpdateMap(OccupancyGridMsg msg)
     {
@@ -92,6 +106,7 @@ class OccupancyMeshGenerator
             rotation = Quaternion.Euler(rotation.eulerAngles + tfFrame.rotation.eulerAngles);
 
             transformUpdateDelegate.Invoke(drawOrigin, rotation);
+            handle = null;
         }
 
         StartGeneratingMesh(msg);
@@ -104,11 +119,11 @@ class OccupancyMeshGenerator
         int height = (int)msg.info.height;
         float resolution = msg.info.resolution;
 
-        int numVertices = (width + 1) * (height + 1);
+        int numVertices = (width + 1) * (height + 1) * 2;
 
         vertexBuffer = new NativeArray<Vector3>(numVertices, Allocator.Persistent);
         uvBuffer = new NativeArray<Vector2>(numVertices, Allocator.Persistent);
-        triangleBuffer = new NativeArray<int>(width * height * 6, Allocator.Persistent);
+        triangleBuffer = new NativeList<int>(width * height * 12, Allocator.Persistent);
         data = new NativeArray<sbyte>(msg.data, Allocator.Persistent);
 
         MeshGenerationJob job = new MeshGenerationJob
@@ -124,6 +139,7 @@ class OccupancyMeshGenerator
         handle = job.Schedule();
     }
 
+
     struct MeshGenerationJob : IJob
     {
         public int height;
@@ -135,41 +151,115 @@ class OccupancyMeshGenerator
 
         public NativeArray<Vector3> vertexBuffer;
         public NativeArray<Vector2> uvBuffer;
-        public NativeArray<int> triangleBuffer;
+        public NativeList<int> triangleBuffer;
+
+
+        private int numVerticesInLayer;
+        private static int threshold = 50;
 
         public void Execute()
         {
-            int i = 0;
+            numVerticesInLayer = (width + 1) * (height + 1);
+            triangleBuffer.Clear();
+
+            // Layer 1
             for (int y = 0; y <= height; y++)
             {
                 for (int x = 0; x <= width; x++)
                 {
                     Vector3 vertex = new Vector3(x * resolution, 0, y * resolution);
 
-                    int threshold = 50;
-                    if (x < width && y < height && data[x + width * y] > threshold // Check upper right of vertex
-                        || (x > 0 && y < height && data[x - 1 + width * y] > threshold) // Check upper left of vertex
-                        || (x < width && y > 0 && data[x + width * (y - 1)] > threshold) // Check lower right of vertex
-                        || (x > 0 && y > 0 && data[x - 1 + width * (y - 1)] > threshold)) // Check lower left of vertex
-                    {
-                        vertex.y = 0.4f;
-                    }
-
                     vertexBuffer[x + y * (width + 1)] = vertex;
                     uvBuffer[x + y * (width + 1)] = new Vector2(x / width, 1 - y / height);
 
-                    if (y < height && x < width)
+                    if (y < height && x < width && data[x + width * y] <= threshold)
                     {
-                        triangleBuffer[6 * i] = x + y * (width + 1);
-                        triangleBuffer[6 * i + 1] = x + (y + 1) * (width + 1);
-                        triangleBuffer[6 * i + 2] = x + 1 + y * (width + 1);
+                        triangleBuffer.Add(x + y * (width + 1));
+                        triangleBuffer.Add(x + (y + 1) * (width + 1));
+                        triangleBuffer.Add(x + 1 + y * (width + 1));
 
-                        triangleBuffer[6 * i + 3] = x + (y + 1) * (width + 1);
-                        triangleBuffer[6 * i + 4] = x + 1 + (y + 1) * (width + 1);
-                        triangleBuffer[6 * i + 5] = x + 1 + y * (width + 1);
-                        i++;
+                        triangleBuffer.Add(x + (y + 1) * (width + 1));
+                        triangleBuffer.Add(x + 1 + (y + 1) * (width + 1));
+                        triangleBuffer.Add(x + 1 + y * (width + 1));
                     }
                 }
+            }
+
+            // Layer 2
+            for (int y = 0; y <= height; y++)
+            {
+                for (int x = 0; x <= width; x++)
+                {
+                    Vector3 vertex = new Vector3(x * resolution, 0.4f, y * resolution);
+
+                    vertexBuffer[x + y * (width + 1) + numVerticesInLayer] = vertex;
+                    uvBuffer[x + y * (width + 1) + numVerticesInLayer] = new Vector2(x / width, 1 - y / height);
+
+                    if (y < height && x < width && data[x + width * y] > threshold)
+                    {
+                        triangleBuffer.Add(x + y * (width + 1) + numVerticesInLayer);
+                        triangleBuffer.Add(x + (y + 1) * (width + 1) + numVerticesInLayer);
+                        triangleBuffer.Add(x + 1 + y * (width + 1) + numVerticesInLayer);
+
+                        triangleBuffer.Add(x + (y + 1) * (width + 1) + numVerticesInLayer);
+                        triangleBuffer.Add(x + 1 + (y + 1) * (width + 1) + numVerticesInLayer);
+                        triangleBuffer.Add(x + 1 + y * (width + 1) + numVerticesInLayer);
+
+                        AddVerticalWalls(x, y);
+                    }
+                }
+            }
+        }
+
+        // Only call for values above threshold
+        private void AddVerticalWalls(int x, int y)
+        {
+            // Check left of point
+            if (x > 0 && data[x - 1 + y * width] <= threshold)
+            {
+                triangleBuffer.Add(x + y * (width + 1));
+                triangleBuffer.Add(x + (y + 1) * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + y * (width + 1) + numVerticesInLayer);
+
+                triangleBuffer.Add(x + (y + 1) * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + y * (width + 1));
+                triangleBuffer.Add(x + (y + 1) * (width + 1));
+            }
+
+            // Check below point
+            if (y > 0 && data[x + (y - 1) * width] <= threshold)
+            {
+                triangleBuffer.Add(x + y * (width + 1));
+                triangleBuffer.Add(x + y * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + 1 + y * (width + 1) + numVerticesInLayer);
+
+                triangleBuffer.Add(x + y * (width + 1));
+                triangleBuffer.Add(x + 1 + y * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + 1 + y * (width + 1));
+            }
+
+            // Check right of point
+            if (x < width - 1 && data[x + 1 + y * width] <= threshold)
+            {
+                triangleBuffer.Add(x + 1 + y * (width + 1));
+                triangleBuffer.Add(x + 1 + y * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + 1 + (y + 1) * (width + 1) + numVerticesInLayer);
+
+                triangleBuffer.Add(x + 1 + (y + 1) * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + 1 + (y + 1) * (width + 1));
+                triangleBuffer.Add(x + 1 + y * (width + 1));
+            }
+
+            // Check above point
+            if (y < height - 1 && data[x + (y + 1) * width] <= threshold)
+            {
+                triangleBuffer.Add(x + (y + 1) * (width + 1));
+                triangleBuffer.Add(x + 1 + (y + 1) * (width + 1) + numVerticesInLayer);
+                triangleBuffer.Add(x + (y + 1) * (width + 1) + numVerticesInLayer);
+
+                triangleBuffer.Add(x + (y + 1) * (width + 1));
+                triangleBuffer.Add(x + 1 + (y + 1) * (width + 1));
+                triangleBuffer.Add(x + 1 + (y + 1) * (width + 1) + numVerticesInLayer);
             }
         }
     }
