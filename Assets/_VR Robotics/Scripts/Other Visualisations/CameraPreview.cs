3f0f91d6c6f236b7c7a209d5dffee287c1db90cb
Alistair Foggin <ajf582@york.ac.uk>
2023-08-07 14:17:31 +0100

    Make screen use compressed image

--- Assets/_VR Robotics/Scripts/Other Visualisations/CameraPreview.cs
+++ Assets/_VR Robotics/Scripts/Other Visualisations/CameraPreview.cs
@@ -5,33 +5,20 @@ using UnityEngine;
 public class CameraPreview : MonoBehaviour
 {
     ROSConnection m_ROSConnection;
-    Material m_Material;
     Texture2D m_Texture;
 
     // Start is called before the first frame update
     void Start()
     {
         m_ROSConnection = ROSConnection.GetOrCreateInstance();
-        m_Material = GetComponent<MeshRenderer>().material;
+        m_Texture = new Texture2D(2, 2);
+        GetComponent<MeshRenderer>().material.mainTexture = m_Texture;
 
-        m_ROSConnection.Subscribe<ImageMsg>("/camera/image_raw", UpdateImage);
+        m_ROSConnection.Subscribe<CompressedImageMsg>("/camera/image_raw/compressed", UpdateImage);
     }
 
-    private void UpdateImage(ImageMsg msg)
+    private void UpdateImage(CompressedImageMsg msg)
     {
-        if (m_Texture == null)
-        {
-            m_Texture = new Texture2D((int)msg.width, (int)msg.height, TextureFormat.RGB24, false);
-            m_Texture.wrapMode = TextureWrapMode.Clamp;
-            m_Texture.filterMode = FilterMode.Trilinear;
-            m_Material.mainTexture = m_Texture;
-        }
-        else if (msg.width != m_Texture.width || msg.height != m_Texture.height)
-        {
-            m_Texture.Reinitialize((int)msg.width, (int)msg.height);
-        }
-
-        m_Texture.SetPixelData(msg.data, 0);
-        m_Texture.Apply(false);
+        ImageConversion.LoadImage(m_Texture, msg.data);
     }
 }
